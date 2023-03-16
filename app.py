346e515ef33edaf4a00266a96c6420e713a2701c
Thomas Fogarty <t.fogarty24@outlook.com>
2023-03-16 19:41:33 +0000

    html files served

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Flask test worked"
+def index():
+    return render_template("index.html")
+
+@app.route("/basic")
+def testing():
+    return render_template("basic.html")
+
+@app.route("/advanced")
+def advanced():
+    return render_template("advanced.html")
+
+@app.route("/creative")
+def creative():
+    return render_template("creative.html")
