e8bf68ff4d4500462c6ddf18440bd3d6f8544c19
Thomas Fogarty <t.fogarty24@outlook.com>
2023-03-17 19:53:31 +0000

    loading csv

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import csv
+import itertools
 
 app = Flask(__name__)
 
@@ -8,8 +9,29 @@ def index():
     return render_template("index.html")
 
 @app.route("/basic")
-def testing():
-    return render_template("basic.html")
+def open():
+     with open('data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8-sig') as csv_file:
+        data = csv.reader(csv_file, delimiter=",")
+        first_line = True
+        tweetData = []
+
+        for row in itertools.islice(data, 51):
+            if not first_line:
+                tweetData.append({
+                    "id": row[0],
+                    "airline_sentiment": row[1],
+                    "airline_sentiment_confidence": row[2],
+                    "negative_reason": row[3],
+                    "airline": row[4],
+                    "name": row[5],
+                    "text": row[6],
+                    "tweet_created": row[7],
+                    "tweet_location": row[8]
+                })
+            else:
+                first_line = False
+        return render_template("basic.html", tweetData=tweetData)
+
 
 @app.route("/advanced")
 def advanced():
