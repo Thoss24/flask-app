761eac0caf6215d67578e018445961371edf9022
Thomas Fogarty <t.fogarty24@outlook.com>
2023-03-21 20:58:39 +0000

    basic.html table sorted according to assignment specifications

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from operator import itemgetter
 import csv
 import itertools
+import pandas as pandasForSortingCSV
 
 app = Flask(__name__)
 
@@ -13,10 +14,11 @@ def index():
 def display_data():
      with open('static/data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8') as csv_file:
         data = csv.reader(csv_file, delimiter=",")
+        sorted_list = sorted(data, key=lambda line: line[2], reverse=False)
         first_line = True
         tweetData = []
 
-        for row in itertools.islice(data, 41):
+        for row in itertools.islice(sorted_list, 41):
             if not first_line:
                 tweetData.append({
                     "id": row[0],
