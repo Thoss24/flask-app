4090f7d21ce7e8928c7dba8230f662b5a0435e0e
Thomas Fogarty <t.fogarty24@outlook.com>
2023-03-31 13:23:10 +0100

    tests implemented

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def display_data():
                     "text": row[6],
                     "tweet_created": row[7]
                 })
-                print(tweetData)
             else:
                 first_line = False
         return render_template("basic.html", tweetData=tweetData)
--- test_assign_two.py
+++ test_assign_two.py
@@ -1,12 +1,18 @@
 import unittest
-from test_code import testing
+from test_code import data_length
+from test_code import bubbleSort
 
 class assignmentTests(unittest.TestCase):
 
     def test_one(self):
         message = "failed"
-        self.assertEqual(testing(2, 2), 4, message)
+        self.assertEqual(data_length([1, 2, 3, 4, 5, 6]), 6, message)
+    # check length of array, to ensure 40 rows of data are being displayed
 
+    def test_two(self):
+        message = "failed"
+        self.assertEqual(bubbleSort([0.465, 0.765, 0.656, 0.265]), [0.265, 0.465, 0.656, 0.765], message)
+    # this test checks if the airline sentiment confidence column is in ascending order
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
